a6081560cf2d3f3f0b776b8fbb5dd70987135275
kamilapu <kamilapuzyrova@gmail.com>
2024-05-17 20:03:38 +0100

    offer in progress

--- src/pages/landing/offer.jsx
+++ src/pages/landing/offer.jsx
@@ -1,7 +1,19 @@
 export const OfferLanding = () => {
-    return (
-      <div>
-          OfferLandingg
+  return (
+    <div className="offerLanding">
+
+      <div className="offerText">
+        <p className="offerText__title">Offer</p>
+        <h2 className="offerText__desc">We Offer Organic For You</h2>
+        <a href="/" className="offerText__btn">View All Product</a>
       </div>
-    );
-  }
+
+      <div className="offerCards">
+        <div className="offerCards__card">
+          <div className="offerCards__img"></div>
+          <div className="offerCards__price"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
