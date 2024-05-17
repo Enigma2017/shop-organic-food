36b519af2c75c0711dc6bdc499f7af22511fb947
Yarinjo <gladysyora@gmail.com>
2024-05-18 01:58:29 +0300

    deleted all and added 2 pictures in discount

--- src/pages/landing/discount.jsx
+++ src/pages/landing/discount.jsx
@@ -1,24 +1,15 @@
-import DiscountImage1 from '/discount-image1.png'
-import DiscountImage2 from '/discount-image2.png'
+import DiscountImageBlock1 from '/discount-image-block1.png'
+import DiscountImageBlock2 from '/discount-image-block2.png'
 
 export const DiscountLanding = () => {
     return (
       <section className='discount'>
           <div className="discount__image-block">
-            <img src={DiscountImage1} alt="discount-image"/> 
-            <div className="discount__image-block-text discount-text-block1">
-              <span>Natural!!</span>
-              <p>Get Garden <br /> Fresh Fruits</p>
-            </div>
+            <img src={DiscountImageBlock1} alt="DiscountImageBlock1" />
           </div>
           <div className="discount__image-block">
-            <img src={DiscountImage2} alt="discount-image"/> 
-            <div className="discount__image-block-text discount-text-block2">
-              <span>Offer!!</span>
-              <p>Get 10% off <br /> on Vegetables</p>
-            </div>
+            <img src={DiscountImageBlock2} alt="DiscountImageBlock2" />
           </div>
-          
       </section>
     );
   }
