e1553b60f1637e2042de55fc9dcaf176d95f226f
Yarinjo <gladysyora@gmail.com>
2024-05-17 12:12:01 +0300

    added discounts block

--- src/pages/landing/discount.jsx
+++ src/pages/landing/discount.jsx
@@ -1,7 +1,24 @@
+import DiscountImage1 from '/discount-image1.png'
+import DiscountImage2 from '/discount-image2.png'
+
 export const DiscountLanding = () => {
     return (
-      <div>
-          DiscountLanding
-      </div>
+      <section className='discount'>
+          <div className="discount__image-block">
+            <img src={DiscountImage1} alt="discount-image"/> 
+            <div className="discount__image-block-text discount-text-block1">
+              <span>Natural!!</span>
+              <p>Get Garden Fresh Fruits</p>
+            </div>
+          </div>
+          <div className="discount__image-block">
+            <img src={DiscountImage2} alt="discount-image"/> 
+            <div className="discount__image-block-text discount-text-block2">
+              <span>Offer!!</span>
+              <p>Get 10% off <br /> on Vegetables</p>
+            </div>
+          </div>
+          
+      </section>
     );
   }
