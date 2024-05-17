d52283e15c208300024ac12758ba08ac914bf7cb
Random4eg <panaram452@gmail.com>
2024-05-17 16:00:13 +0300

    added responsive mode

--- src/pages/landing/about_us.jsx
+++ src/pages/landing/about_us.jsx
@@ -1,7 +1,52 @@
+import Food from "/about/food.svg"
+import Mailbox from "/about/mailbox.svg"
+import Arrow from "/about/arrow.svg"
 export const AboutUsLanding = () => {
-    return (
-      <div>
-          AboutUsLanding
+  return (
+    <section className="container">
+      <div className="about__container">
+        <div className="about__img">
+          <img src="/about/fruit.png" alt="Photo" />
+        </div>
+        <div className="about__box">
+          <div className="about__title">About Us</div>
+          <h2 className="about__desc">
+            We Believe in Working Accredited Farmers{" "}
+          </h2>
+          <p>
+            Simply dummy text of the printing and typesetting industry. Lorem
+            had ceased to been the industry's standard dummy text ever since the
+            1500s, when an unknown printer took a galley.
+          </p>
+          <div className="about__item">
+            <div className="about__icon">
+              <img src={Food} alt="vegan food" />
+            </div>
+            <div className="about__smalldesc">
+              <h6 className="about__smalltitle">Quality Standards</h6>{" "}
+              <p>
+                Simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum
+              </p>
+            </div>
+          </div>
+          <div className="about__item">
+            <div className="about__icon">
+              <img src={Mailbox} alt="Mailbox" />
+            </div>
+            <div className="about__smalldesc">
+              <h6 className="about__smalltitle">Quality Standards</h6>{" "}
+              <p>
+                Simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum
+              </p>
+            </div>
+          </div>
+          <button className="about__btn" type="submit">
+            Shop Now <img src={Arrow} alt="arrow" />
+          </button>
+        </div>
       </div>
-    );
-  }
+    </section>
+  )
+}
