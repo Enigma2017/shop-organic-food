783acfa034b4a670c5fc3fe664cddea2f1660474
kamilapu <kamilapuzyrova@gmail.com>
2024-05-19 14:25:03 +0100

    offer ready

--- src/pages/landing/offer.jsx
+++ src/pages/landing/offer.jsx
@@ -1,4 +1,10 @@
+import { ProductCard } from '../../shared/product-card';
+import productData from '../../data/products-items.json';
+import { ArrowIcon } from '../../shared/icons/arrowIcon';
+
+
 export const OfferLanding = () => {
+  const productList = productData.slice(12, 17);
   return (
     <section className="offerLanding">
 
@@ -10,34 +16,16 @@ export const OfferLanding = () => {
         </div>
 
         <div className="offerButton">
-          <a href="/" className="offerButton__btn">View All Product <svg className="offerButton__btnArrow" viewBox="0 0 9 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M4.65708 1.12891L8.05162 4.19342L4.65708 7.25794M7.58015 4.19342H0.696777" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
-          </svg></a>
+          <a href="/" className="offerButton__btn">View All Product <span className='offerButton__btnArrow'><ArrowIcon /></span></a>
         </div>
 
       </div>
 
       <div className="offerCards">
 
-        <div className="offerCards__card">
-          <div className="offerCards__img">ImgExample</div>
-          <div className="offerCards__price">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi voluptatem labore quidem laudantium tempore iste, sunt velit perspiciatis temporibus maiores repellendus, ex, vitae aliquam quia consectetur! Odio placeat hic at.</div>
-        </div>
-
-        <div className="offerCards__card">
-          <div className="offerCards__img">ImgExample</div>
-          <div className="offerCards__price">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi voluptatem labore quidem laudantium tempore iste, sunt velit perspiciatis temporibus maiores repellendus, ex, vitae aliquam quia consectetur! Odio placeat hic at.</div>
-        </div>
-
-        <div className="offerCards__card">
-          <div className="offerCards__img">ImgExample</div>
-          <div className="offerCards__price">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi voluptatem labore quidem laudantium tempore iste, sunt velit perspiciatis temporibus maiores repellendus, ex, vitae aliquam quia consectetur! Odio placeat hic at.</div>
-        </div>
-
-        <div className="offerCards__card">
-          <div className="offerCards__img">ImgExample</div>
-          <div className="offerCards__price">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi voluptatem labore quidem laudantium tempore iste, sunt velit perspiciatis temporibus maiores repellendus, ex, vitae aliquam quia consectetur! Odio placeat hic at.</div>
-        </div>
+        {productList.map((item, index) => (
+          <ProductCard key={item.id || index} item={item} />
+        ))}
 
       </div>
     </section>
