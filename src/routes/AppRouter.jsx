50763e883154ce78f8e4f894b896c7ab0c5e6864
Protypes <protypes773@gmail.com>
2024-05-19 19:59:47 +0300

    team page ready for development / added route

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { PageNotFound } from "../pages/utility_pages/404";
+
+import { TeamPage } from "../pages/team/team-main";
 import { Landing } from "../pages/landing/landing-main";
 import { AboutUsPage } from "../pages/about-us/aboutus-main";
-import { PageNotFound } from "../pages/utility_pages/404";
 
 export const AppRoutes = () => {
     return (
@@ -9,6 +12,7 @@ export const AppRoutes = () => {
         <Routes>
           <Route index={true} path="/" element={<Landing />} />
           <Route index={true} path="/aboutus" element={<AboutUsPage />} />
+          <Route index={true} path="/team" element={<TeamPage />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
     </BrowserRouter>
