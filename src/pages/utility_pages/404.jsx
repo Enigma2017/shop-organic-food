dc49b59f5f7ede71fda1cd4d7ca2c3cbb8b5aa57
Random4eg <panaram452@gmail.com>
2024-05-20 20:12:58 +0300

    added quality page

--- src/pages/utility_pages/404.jsx
+++ src/pages/utility_pages/404.jsx
@@ -1,18 +1,15 @@
 import Arrow from "/about/arrow.svg"
-import Error from "/public/404/error.png"
-// import Pattern from "/public/404/patterns.svg"
-// import Pattern1 from "/public/404/patterns1.svg"
-// import Pattern2 from "/public/404/patterns2.svg"
-// import Pattern3 from "/public/404/patterns3.svg"
+import Photo from "/public/404/photo.png"
 
 export const PageNotFound = () => {
   return (
     <section className="notfound">
       <div className="notfound__container">
-        <div className="notfound__info">
-          <div className="notfound__slogan">
-            <img src={Error} alt="Error" />
-          </div>
+        <div className="notfound__img">
+          <img src={Photo} alt="Photo" />
+        </div>
+        <div className="notfound_item">
+          <div className="notfound__slogan">404</div>
           <div className="notfound__title">Page not found</div>
           <div className="notfound__desc">
             The page you are looking for doesn&#39;t exist or has been moved
@@ -21,18 +18,6 @@ export const PageNotFound = () => {
             Go to Homepage <img src={Arrow} alt="arrow" />
           </a>
         </div>
-        {/* <div className="notfound__patterns">
-          <img src={Pattern} alt="Pattern" />
-        </div>
-        <div className="notfound__patterns1">
-          <img src={Pattern1} alt="Pattern" />
-        </div>
-        <div className="notfound__patterns2">
-          <img src={Pattern2} alt="Pattern" />
-        </div>
-        <div className="notfound__patterns3">
-          <img src={Pattern3} alt="Pattern" />
-        </div> */}
       </div>
     </section>
   )
